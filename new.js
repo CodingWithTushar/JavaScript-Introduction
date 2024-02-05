e97494bc521a3a00029879a5ba32b272b8e82045
Tushar <tushard3998@gmail.com>
2024-02-05 11:34:50 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,4 @@
+console.log("Code Is Running....")
+console.log("Code Is also Running....")
+console.log("Code Is Running very fast....")
+console.log("Hello World")
---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Hello World")
+
+console.log("Code Is Running....")
+console.log("Code Is also Running....")
+console.log("Code Is Running very fast....")
+console.log("Code Is Running just like a wow....")
+
+var a = prompt("Enter Your Number" + a )
+
+var isTrue = confirm("Are You Sure You Want To Leave Your Page And Blast Your Computer")
+
+if(isTrue){
+    console.log("Computer Is Blast")
+}
+console.log("Your Number Is" + a)
+
+document.title = "Hey  Im a Good Boy"
+
+document.body.style.backgroundColor ="red"
